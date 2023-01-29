c448da23b1044622221a64911af788903b1c7967
marcelocarvalholopes <marcelolcarvalho@gmail.com>
2023-01-29 13:54:34 -0300

    conexão com mongo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
-let express = require("express");
-let     app = express();
+let express  = require("express");
+let     app  = express();
+let mongoose = require("mongoose");
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+
+mongoose.set('strictQuery', false);
+
+mongoose.connect("mongodb://0.0.0.0:27017/guiapics",{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('connected to db')
+    }).catch((err) => {
+        console.log(err.message);
+    });
 
 
 app.get("/",(req, res) => {
